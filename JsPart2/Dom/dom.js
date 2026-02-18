c9bcffaf302b63d903ba4e34913eab65b0fd23a6
amit yadav <amit1development@gmail.com>
2026-02-19 03:56:08 +0530

    change folder structure

--- JsPart2/Dom/dom.js
+++
@@ -1,9 +0,0 @@
-// what is the DOM? How does it represent the Html structure.
-// Name the type of nodes in the dom tree
-// what is diffence element node and text node
-// inspect the following HTML in hte browser and identify each node;
-// what is the diffence b/w getElementById and querySelector?
-// what does getElementByClass return? is it an array // it's not an array, it's html collection.
-
-// use queryselctorAll to select all buttons with class by-now.
-
