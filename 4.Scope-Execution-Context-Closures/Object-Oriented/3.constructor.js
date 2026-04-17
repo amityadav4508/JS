e625b138100f10c31b4229637318d8ce27f80479
Amit Yadav <amity4508@gamil.com>
2026-04-17 05:40:00 +0530

    file updates

--- 4.Scope-Execution-Context-Closures/Object-Oriented/3.constructor.js
+++ 4.Scope-Execution-Context-Closures/Object-Oriented/3.constructor.js
@@ -39,32 +39,3 @@ let pencil2 = new CreatePencil("Doms", 5, "red", "coms")
 
 
 
-// Portotypes - means extra memory or shared memory
-
-// agar tumhar constructor function koi fiel apne prototype par attach karle to us constructor se banane wale sabhi new instance yani ki objects ke pass wo filed automatically chali jati hai. 
-
-// ye 
-
-function CreatePencilP(name, price, color, company){
-    this.name = name;
-    this.price = price;
-    this.color = color;
-    this.company = "sheryians";
-  
-}
-
-CreatePencilP.prototype.write = function(text){
-        let h1 = document.createElement("h1");
-        h1.textContent = text;
-        h1.style.color = this.color;
-        document.body.appendChild(h1);
-    }
-
-CreatePencilP.prototype.company = "sheryians";
-
-
-let pencilP1 = new CreatePencilP("Nataraj", 10, "blue", "nataraj");
-
-let pencilP2 = new CreatePencilP("Doms", 5, "red", "coms")
-
-
