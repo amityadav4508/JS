78397d7b15b5eb79f4bd0342bd016b0bbf01eb02
Amit Yadav <amity4508@gamil.com>
2026-04-22 04:34:40 +0530

    sync&async

---
+++ 6.sync&async/sync&async.js
@@ -0,0 +1,8 @@
+
+/* koi bhi code js mein line by line chalega aur ye natural pattern bhi hota hai ki code line by line chale, but kabhi kabar asise cases aate hai life mein jaha par aapka code wait krta hai and utni der mein agla code chal jatta hai.
+
+
+
+Sync/async
+aisa code jo line by line chale hot hai sync code
+ausa code jo jab chalne ke liye ready ho jaaye tab chale wo hai async   */
