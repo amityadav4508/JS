773d576f02d9849e1e2eb7428af5ec70d834b60f
Amit Yadav <amity4507@gamil.com>
2026-04-24 05:49:56 +0530

    new update

---
+++ 9.System Design/Design_Patter/3.FactoryfunctionPattern/factoryPattern.js
@@ -0,0 +1,16 @@
+function createProduct(name, price){
+    let stock = 10;
+    return{
+        name,
+        price,
+        checkStock(){
+            console.log(stock)
+        },
+        buy(qty){
+            if(qty<=stock){
+                stock -= qty;
+                console.log(`${qty} pices booked - ${stock} pieces left.`)
+            }
+        }
+    }
+}
